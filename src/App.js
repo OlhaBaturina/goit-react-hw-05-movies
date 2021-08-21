a0c8f4cddbbb6d0d7f6171aa7365936982c1952d
Olha Baturina <75169470+OlhaBaturina@users.noreply.github.com>
2021-08-21 22:22:36 +0300

    Добавляет стили списку

--- src/App.js
+++ src/App.js
@@ -3,12 +3,6 @@ import { toast, ToastContainer } from 'react-toastify';
 import { Switch, Route } from 'react-router';
 import 'react-toastify/dist/ReactToastify.css';
 import s from './App.css';
-// import Searchbar from './Components/OldImagec/Searchbar/Searchbar';
-// import ImageGallery from './Components/ImageGallery/ImageGallery';
-// import CustomLoader from './Components/OldImagec/Loader/Loader';
-// import Button from './Components/OldImagec/Button/Button';
-// import Modal from './Components/OldImagec/Modal/Modal';
-// import { fetchImgAPI } from './servises/useFetch';
 import Tour from './Components/Tour/Tour';
 import Navigation from './Components/Navigation/Navigation';
 import HomePage from './views/HomePage';
@@ -19,25 +13,6 @@ import { fetchAPI } from './servises/useFetch';
 import TrendsMovie from './Components/TrendsMovie/TrendsMovie';
 
 export function App() {
-    //     const [movies, setMovies] = useState([]);
-
-    // const fetchTrends = async () => {
-
-    //     try {
-    //         const trendsRes = await fetchAPI();
-    //         console.log(trendsRes);
-    //         setMovies(prevState => [...prevState, ...trendsRes]);
-
-    //     } catch (error) {
-
-    //         console.log(error)
-    //     }
-    // };
-
-    // fetchTrends()
-
-    // console.log(fetchAPI())
-
     return (
         <div className={s.App}>
             <ToastContainer />
