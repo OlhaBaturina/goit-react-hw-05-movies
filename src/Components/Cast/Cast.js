8966416643d7710c5b5cb3715fb33cd51b886872
Olha Baturina <75169470+OlhaBaturina@users.noreply.github.com>
2021-08-26 18:58:47 +0300

    Добавляет рендер по статусу в каст и ревью

--- src/Components/Cast/Cast.js
+++ src/Components/Cast/Cast.js
@@ -2,38 +2,49 @@ import s from './Cast.module.css';
 import { useParams, useLocation, useHistory } from 'react-router';
 import { useState, useEffect } from 'react';
 import { fetchAPI } from '../../servises/useFetch';
+import CustomLoader from '../Loader/Loader';
 
 export default function CastDetails() {
     const { movieId } = useParams();
     const location = useLocation();
     const history = useHistory();
     const [cast, setCast] = useState([]);
+    const [status, setStatus] = useState('pending');
 
     useEffect(() => {
-        fetchAPI.getCast(movieId).then(setCast);
+        setStatus('loading');
+        fetchAPI
+            .getCast(movieId)
+            .then(r => {
+                console.log(r);
+                setCast(r);
+                setStatus('pending');
+            })
+            .catch(error => error && setStatus('rejected'));
     }, [movieId]);
 
     return (
         <ul className={s.list}>
-            {cast.length > 0
-                ? cast.map(({ profile_path, character, id, name }) => (
-                      <li key={id} className={s.listItem}>
-                          <div>
-                              <img
-                                  src={
-                                      profile_path
-                                          ? `https://image.tmdb.org/t/p/w200${profile_path}`
-                                          : 'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled.png'
-                                  }
-                                  alt={name}
-                                  width="200"
-                              />
-                          </div>
-                          <h3>{name}</h3>
-                          <p>{character}</p>
-                      </li>
-                  ))
-                : 'No information about cast'}
+            {(status === 'pending' &&
+                cast.map(({ profile_path, character, id, name }) => (
+                    <li key={id} className={s.listItem}>
+                        <div>
+                            <img
+                                src={
+                                    profile_path
+                                        ? `https://image.tmdb.org/t/p/w200${profile_path}`
+                                        : 'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled.png'
+                                }
+                                alt={name}
+                                width="200"
+                            />
+                        </div>
+                        <h4>{name}</h4>
+                        <p>{character}</p>
+                    </li>
+                ))) ||
+                (status === 'loading' && <CustomLoader />) ||
+                (status === 'rejected' && <h3>No information about cast.</h3>)}
         </ul>
     );
 }
--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -2,26 +2,40 @@ import s from './Reviews.module.css';
 import { useParams } from 'react-router';
 import { useState, useEffect } from 'react';
 import { fetchAPI } from '../../servises/useFetch';
+import CustomLoader from '../Loader/Loader';
 
 export default function ReviewsDetails() {
     const { movieId } = useParams();
-
+    const [status, setStatus] = useState('pending');
     const [reviews, setReviews] = useState([]);
 
     useEffect(() => {
-        fetchAPI.getReviews(movieId).then(setReviews);
+        setStatus('loading');
+        fetchAPI
+            .getReviews(movieId)
+            .then(r => {
+                console.log(r);
+                setReviews(r);
+                setStatus('pending');
+            })
+            .catch(error => error && setStatus('rejected'));
     }, [movieId]);
 
     return (
         <ul>
-            {reviews.length > 0
-                ? reviews.map(({ author, content, id }) => (
-                      <li key={id} className={s.listItem}>
-                          <h3>Author: {author}</h3>
-                          <p>{content}</p>
-                      </li>
-                  ))
-                : "We don't have any reviews for this movie."}
+            {(status === 'pending' &&
+                reviews.map(({ author, content, id }) => (
+                    <li key={id} className={s.listItem}>
+                        <h4>
+                            Author: <span> {author}</span>
+                        </h4>
+                        <p>{content}</p>
+                    </li>
+                ))) ||
+                (status === 'loading' && <CustomLoader />) ||
+                (status === 'rejected' && (
+                    <h3>We don't have any reviews for this movie.</h3>
+                ))}
         </ul>
     );
 }
